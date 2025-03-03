5796ade4e694653d533d8d08bbb52652a5712b3f
SazanamiInko <s.suzuki1614@gmail.com>
2025-03-03 17:14:20 +0900

    pg002_func作成

---
+++ pg002_func/pg002_func.dpr
@@ -0,0 +1,17 @@
+program pg002_func;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+    { TODO -oUser -cConsole メイン : ここにコードを記述してください }
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
